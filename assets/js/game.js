c29d9466521b432a53da2a69e23afcd30bcb9af5
Jason <jasonpsmith180@gmail.com>
2021-02-01 22:16:38 -0700

    added test js code

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
-window.alert("This is an alert! JavaScript is running!")
+// THIS CREATES A PROMPT TO ASK FOR THE PLAYER ROBOT'S NAME
+var playerName = window.prompt("What is your robot's name?");
+    console.log(playerName)
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robots's name is " + playerName)
+
+
+// THIS CREATES A FUNCTION NAMED "FIGHT"
+function fight() {
+    window.alert("The fight has begun!")
+}
+
+// fight();
