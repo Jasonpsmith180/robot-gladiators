340d2ba1a6828b2b0b49011cfe91b52e943805a9
Jason <jasonpsmith180@gmail.com>
2021-02-05 20:45:16 -0700

    fixed null name bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -90,7 +90,6 @@ var shop = function()
         case "LEAVE":
         case "leave":
             window.alert("Leaving the store.");
-
             // Do nothing so function will end
             break;
 
@@ -180,10 +179,22 @@ var fight = function(enemy)
     }
 };
 
+// Function to set name
+var getPlayerName = function()
+{
+    var name = "";
+    while (name === "" || name === null)
+    {
+        name = prompt("What is your robot's name?");
+    }
+
+    console.log("Your robot's name is " + name);
+    return name;
+};
 // This creates a prompt to ask for the player robot's name and establishes health and attack values
 var playerInfo = 
 {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
